a632db2290002a5ad31efc8832c9b97cfe390423
Eduardo2486 <eduardo0137@protonmail.com>
2021-06-18 17:09:11 -0500

    pruebas equipos

---
+++ js.js
@@ -0,0 +1,29 @@
+/*
+Recibes la tarea de construir un sistema de alarma para una fábrica de champús.
+Tu sistema recibe las siguientes variables: 
+  temperatura ambiente, 
+  humedad ambiental, 
+  número de trabajadores en la planta y 
+  nivel de líquido en los tanques. 
+La alarma debe sonar siempre que la temperatura ambiente sea superior o inferior a los umbrales, 
+si la humedad es superior al 50%, si hay más de 100 trabajadores en la planta al mismo tiempo, 
+o si el nivel del líquido es superior al 90% (para evitar el desbordamiento).
+
+umbral superior 90% umbral inferior 30%
+*/
+
+/*
+Escribir un programa que pida al usuario su peso (en kg) y estatura (en metros), 
+calcule el índice de masa corporal y lo almacene en una variable, 
+y muestre por pantalla la frase Tu índice de masa corporal es <imc> donde <imc> es el índice de masa corporal calculado redondeado con dos decimales.
+
+*/
+
+
+
+/*
+
+Escribir un programa que lea un entero positivo, n , 
+introducido por el usuario y después muestre en pantalla la suma de todos los enteros desde 1 hasta n
+*/
+
